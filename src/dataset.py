34bc1d6beae6e7a0772888ab7ceef4d1df7a2059
oldblue98 <takahashikeiji@takahashikyoujinoMacBook-Air.local>
2021-08-20 12:33:45 +0900

    add channel domain

--- src/dataset.py
+++ src/dataset.py
@@ -12,11 +12,12 @@ from nnAudio.Spectrogram import CQT1992v2
 
 
 class ImageDataset(Dataset):
-    def __init__(self, train_df, transforms=None):
+    def __init__(self, train_df, transforms=None, image_type="spatial"):
 
         self.image_paths = train_df["image_path"].values
         self.labels = train_df["label"].values
         self.augmentations = transforms
+        self.image_type = image_type
 
     def __len__(self):
         return len(self.image_paths)
@@ -26,7 +27,7 @@ class ImageDataset(Dataset):
         target = self.labels[index]
 
         image = np.load(image_path)
-        image = self.apply_qtransform(image)
+        image = self.apply_qtransform(image, self.image_type)
         # image = cv2.resize(image, (image.shape[0]//2, image.shape[0]//2))
         image = image.squeeze().numpy()
         if self.augmentations:
@@ -35,9 +36,15 @@ class ImageDataset(Dataset):
 
         return image, torch.tensor(target)
 
-    def apply_qtransform(self, waves, transform=CQT1992v2(sr=2048, fmin=20, fmax=1024, hop_length=64)):
-        waves = np.hstack(waves)
-        waves = waves / np.max(waves)
-        waves = torch.from_numpy(waves).float()
-        image = transform(waves)
+    def apply_qtransform(self, waves, image_type, transform=CQT1992v2(sr=2048, fmin=20, fmax=1024, hop_length=64)):
+        if image_type == "spatial":
+            waves = np.hstack(waves)
+            waves = waves / np.max(waves)
+            waves = torch.from_numpy(waves).float()
+            image = transform(waves)
+        elif image_type == "channel":
+            image = np.concatenate([transform(torch.from_numpy(waves[i]/np.max(waves[i])).float()) for i in range(len(waves))], axis=0)
+            image = image.transpose(1, 2, 0)
+        else:
+            raise Exception("image_type is not defined")
         return image
